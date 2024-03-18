44b618a34435741e578a8994e6738a8f959cb45c
hidexxxxx <hidetaka54522@gmail.com>
2024-03-18 11:03:06 +0900

    検索結果の表示まで

--- resources/views/UserProfile/index.blade.php
+++ resources/views/UserProfile/index.blade.php
@@ -11,7 +11,6 @@
     <div class="max-w-7xl mx-auto sm:w-10/12 md:w-8/10 lg:w-8/12">
       <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
         <div class="p-6 bg-white dark:bg-gray-800 border-b border-grey-200 dark:border-gray-800">
-
           <table class="text-center w-full border-collapse">
             <thead>
               <tr>
@@ -20,38 +19,31 @@
             </thead>
             {{-- 一覧表示 --}}
             <tbody>
-
               @foreach ($UserProfiles as $UserProfile)
-
                   <tr class="hover:bg-gray-lighter">
                       <td class="py-4 px-6 border-b border-gray-light dark:border-gray-600">
                         <a href="{{ route('UserProfile.show',$UserProfile->id) }}">
                           <h3 class="text-left font-bold text-lg text-gray-dark dark:text-gray-200">{{$UserProfile->Nickname}}</h3>
                         </a>
                         <div class="flex">
-
-                        <!-- 🔽 条件分岐でログインしているユーザが投稿したのみ編集ボタンが表示される -->
-                        @if ($UserProfile->user_id === Auth::user()->id)
-                          <!-- 編集ボタン -->
-                          <form action="{{ route('UserProfile.edit',$UserProfile->id) }}" method="GET" class="text-left">
-                            @csrf
-                            <x-primary-button class="ml-3">
-                              <svg class="h-6 w-6 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="gray">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
-                              </svg>
-                            </x-primary-button>
-                          </form>
-                        @endif
-                      </div>
-
+                          <!-- 🔽 条件分岐でログインしているユーザが投稿したのみ編集ボタンが表示される -->
+                          @if ($UserProfile->user_id === Auth::user()->id)
+                            <!-- 編集ボタン -->
+                            <form action="{{ route('UserProfile.edit',$UserProfile->id) }}" method="GET" class="text-left">
+                              @csrf
+                              <x-primary-button class="ml-3">
+                                <svg class="h-6 w-6 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="gray">
+                                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                                </svg>
+                              </x-primary-button>
+                            </form>
+                          @endif
+                        </div>
                       </td>
                   </tr>
-
               @endforeach
-
             </tbody>
           </table>
-
         </div>
       </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,13 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserProfileController;
+use App\Http\Controllers\SearchController;
 
 // CRUD処理の一括指定なのでresourceを使う
 // 以下でログインしているユーザーのみ使えるようにする
 Route::middleware('auth')->group(function () {
+    Route::get('/UserProfile/search/input', [SearchController::class, 'create'])->name('search.input');
+    Route::get('/UserProfile/search/result', [SearchController::class, 'index'])->name('search.result');
     Route::get('/UserProfile/mypage', [UserProfileController::class, 'mydata'])->name('UserProfile.mypage');
     Route::resource('UserProfile', UserProfileController::class);
 });
