5b83ca19408dade3ec1db9e3656751a75262d5b2
hidexxxxx <hidetaka54522@gmail.com>
2024-03-31 18:03:42 +0900

    利用規約のリンク設置

---
+++ app/Http/Controllers/TermsController.php
@@ -0,0 +1,24 @@
+<?php
+
+// TermsAndConditionsController.php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+// class TermsController extends Controller
+// {
+//     public function TermsConditions()
+//     {
+//         return view('terms.TermsConditions');
+//     }
+
+//     public function privacyPolicy()
+//     {
+//         return view('PrivacyPolicy');
+//     }
+
+//     public function aboutUs()
+//     {
+//         return view('AboutUs');
+//     }
+// }
