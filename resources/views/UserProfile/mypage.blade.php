a077c378c3f879b040f82edd25863b3cc2be2547
hidexxxxx <hidetaka54522@gmail.com>
2024-04-17 19:30:10 +0900

    imgの読み取り元修正

--- resources/views/UserProfile/mypage.blade.php
+++ resources/views/UserProfile/mypage.blade.php
@@ -116,7 +116,7 @@
 
   <footer class="text-gray-600 body-font">
     <div class="container px-5 py-8 mx-auto flex flex-col sm:flex-row justify-center items-center">
-        <img src="{{ asset('images/LeapCompass.png') }}" class="p-2 hover:cursor-pointer" alt="Logo" style="width:15%; height:auto;" onclick="window.location.href = '/UserProfile'">
+      <img src="{{ asset('images/LeapCompass.png') }}" class="p-2 hover:cursor-pointer" alt="Logo" style="width:15%; height:auto;" onclick="window.location.href = '/LeapCompass360/UserProfile'">
         <div class="flex flex-col sm:flex-row items-center">
         <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 py-2">© 2024 Leap Compass 360 —</p>
 
